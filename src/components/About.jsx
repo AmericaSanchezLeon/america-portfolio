d22013fb8b5051357ca2aaa6a76a001eca801c2d
americasanchezleon <americasanchezleon@gmail.com>
2025-03-24 23:44:51 +0000

    fix | fixed typos in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,13 +26,13 @@ export default function About() {
           <h1 className='about__title' data-text="America Sanchez Leon" tabIndex="0">America Sanchez Leon</h1>
           <h3  tabIndex="0">UX engineer | Product designer | UI developer</h3>
           <p className='mt-3'>I’m an unicorn 🦄
-            I have {uxExperience} years of experience un UX/UI, {webDevExperience} as UI developer and front end developer,
+            I have {uxExperience} years of experience in UX/UI, {webDevExperience} as UI developer and front end developer,
             and a whole background of {designExperience} years as integral designer.
             <br /><br />
             <b> I specialize in reducing the inconsistency between design and development.</b> My goal is to deliver cohesive, functional and usable products. 
             With experience in both design and development, I can
             translate complex user and business needs into
-            intuitive and accesible interfaces that
+            intuitive and accessible interfaces that
             not only meet user expectations but also account
             for engineering constraints, ensuring a smooth transition
             from concept to production. <br /> <br />
